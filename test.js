04cc3b4ee2d0a4bde7c512430b52016ddadfbd37
ihongtaoi <836717428@qq.com>
2018-07-03 22:41:00 +0800

    更新

---
+++ test.js
@@ -0,0 +1,10 @@
+let arr1 = [1, 2, 3, 3, 4, 5, 5, 12, 12, 5, 22, 34];
+
+function arrFliter(arr) {
+    let result = [];
+    console.log(arr.length)
+    for(let i = 0; i < arr.length; i++) {
+        console.log(1);
+    }
+}
+arrFliter(arr1);
