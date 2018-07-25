1fc8a18a70fe28d28ffd66f71bf8e0186a14c79c
ihongtaoi <836717428@qq.com>
2018-07-25 10:50:53 +0800

    设计模式

--- javascript/设计模式/发布-订阅模式.js
+++ javascript/设计模式/发布-订阅模式.js
@@ -1,4 +1,4 @@
-var Event = (function () {
+let Event = (function () {
     let list = {},
         listen,
         trigger,
@@ -9,9 +9,45 @@ var Event = (function () {
             list[key] = [];
         }
         list[key].push(fn);
-    }
+    };
 
     trigger = function () {
-        let key = Array.prototype.shift(arguments);
+        let key = Array.prototype.shift.call(arguments);
+        let msg = list[key];
+        if (!msg || msg.length == 0) {
+            return false;
+        }
+        for (let i = 0; i < msg.length; i++) {
+            msg[i].apply(this, arguments);
+        }
+    };
+
+    remove = function (key, fn) {
+        let msg = list[key];
+        if (!msg) {
+            return false;
+        }
+        if (!fn) {
+            delete list[key];
+        } else {
+            for (let i = 0; i < msg.length; i++) {
+                if (msg[i] == fn) {
+                    msg.splice(i, 1);
+                }
+            }
+        }
+    };
+    return {
+        listen,
+        trigger,
+        remove
     }
-})()
+})();
+
+let fn = function (data) {
+    console.log('lalala' + data);
+}
+
+Event.listen('lalala', fn);
+Event.trigger('lalala', '我是卖报的小行sdasdasd家');
+Event.remove('lalala', fn);
