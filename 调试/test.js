c6a173663057e86e596d8e89931c7f0a24c203f8
ihongtaoi <836717428@qq.com>
2018-07-28 09:34:58 +0800

    插件

--- 调试/test.js
+++ 调试/test.js
@@ -1,8 +1,23 @@
-Number.prototype.toBinarySys = function () {
-  let val = this.valueOf().toString();
-  for (let i = val.length - 1; i >= 0; i--) {
-    console.log(val[i])
+function showPages(page, total, show) {
+  var str = '';
+  if (page < show + 1) {
+    for (var i = 1; i <= show * 2 + 1; i++) {
+      str = str + ' ' + i;
+    }
+  } else if (page > total - show) {
+    for (var i = total - show * 2; i <= total; i++) {
+      str = str + ' ' + i;
+    }
+  } else {
+    for (var i = page - show; i <= page + show; i++) {
+      str = str + ' ' + i;
+    }
   }
+
+  return str.trim();
 }
 
-new Number(123).toBinarySys();
+var total = 30;
+for (var i = 1; i <= total; i++) {
+  console.log(i, showPages(i, total, 2));
+}
